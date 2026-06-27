81e238b3dcf56c637cdff3ec765c32f7ccba4f88
Michael Bestas <mkbestas@lineageos.org>
2026-06-27 03:34:35 +0300

    felix: Update to CP2A.260605.012
    Change-Id: Ia3eed69d8d69982836c1428db848f62b3b90c61c

--- Android.mk
+++ Android.mk
@@ -6,14 +6,14 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),felix)
 
-$(call add-radio-file-sha1-checked,radio/abl.img,75ca02425854ef42763b7f2ed83c4b8ce2202a1b)
-$(call add-radio-file-sha1-checked,radio/bl1.img,4df268570da54669619fa1973c62029fba409505)
-$(call add-radio-file-sha1-checked,radio/bl2.img,ee41fd81e288fedf174a08ed43f5c009c2e448a8)
-$(call add-radio-file-sha1-checked,radio/bl31.img,6e3e620637a7456fb35a0fb52c1d11b409c05718)
-$(call add-radio-file-sha1-checked,radio/gsa.img,d5e1f409085dcff5b0e015b7214a6be79c10d16e)
-$(call add-radio-file-sha1-checked,radio/ldfw.img,76635bc33a0513ebd5de4d48ae2908c76c9e99a4)
-$(call add-radio-file-sha1-checked,radio/modem.img,92207f66f5b1cdbc8530c800d704425349d7a2aa)
-$(call add-radio-file-sha1-checked,radio/pbl.img,c97d710a432c099e48a67796a3710f0bbfa75541)
-$(call add-radio-file-sha1-checked,radio/tzsw.img,27fd462747a44af976645ef7d7df4a16c9a1e2e3)
+$(call add-radio-file-sha1-checked,radio/abl.img,52b3d87cb67d120ff262fdceaba46a0f18eaddaa)
+$(call add-radio-file-sha1-checked,radio/bl1.img,735506b493d614ced75b65a88cc0e0404e777d4b)
+$(call add-radio-file-sha1-checked,radio/bl2.img,e3b2dfbe700ff8f293680060619018bbe3074cba)
+$(call add-radio-file-sha1-checked,radio/bl31.img,d2e24c99fd7baf67ef23209e1e6f4163597eb6f3)
+$(call add-radio-file-sha1-checked,radio/gsa.img,71d0aeb8a455448ddfc9838a2218d99325294351)
+$(call add-radio-file-sha1-checked,radio/ldfw.img,367b2c30ee0ebcd119603c41aa25bbb9ad23096d)
+$(call add-radio-file-sha1-checked,radio/modem.img,e2d31e2c58d51d70de41afe74148ee4319b84e00)
+$(call add-radio-file-sha1-checked,radio/pbl.img,26a4eca8891ddba8f70786095bba7960b94569c3)
+$(call add-radio-file-sha1-checked,radio/tzsw.img,4ad4b6bb8090d3fc4ffe25869b37ef518cca7fce)
 
 endif
